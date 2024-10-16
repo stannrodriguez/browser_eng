67a2e14ba922fb57eb8c44e73c9ac9eb64c925b3
Stephanie Rodriguez <stephsiriban@gmail.com>
2024-10-16 18:09:13 -0400

    add simplified browser for chapter 2

---
+++ chapter2/browser_no_exercises.py
@@ -0,0 +1,84 @@
+"""
+This file compiles the code in Web Browser Engineering,
+up to and including Chapter 2 (Drawing to the Screen),
+without exercises.
+"""
+
+import tkinter
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import wbetools
+from chapter1.browser_no_exercises import URL
+
+def lex(body):
+    text = ""
+    in_tag = False
+    for c in body:
+        if c == "<":
+            in_tag = True
+        elif c == ">":
+            in_tag = False
+        elif not in_tag:
+            text += c
+        wbetools.record("lex", text)
+    return text
+
+WIDTH, HEIGHT = 800, 600
+HSTEP, VSTEP = 13, 18
+
+SCROLL_STEP = 100
+
+def layout(text):
+    display_list = []
+    cursor_x, cursor_y = HSTEP, VSTEP
+    for c in text:
+        display_list.append((cursor_x, cursor_y, c))
+        cursor_x += HSTEP
+        if cursor_x >= WIDTH - HSTEP:
+            cursor_y += VSTEP
+            cursor_x = HSTEP
+        wbetools.record("layout", display_list)
+    return display_list
+
+class Browser:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(
+            self.window,
+            width=WIDTH,
+            height=HEIGHT
+        )
+
+        self.canvas.pack()
+
+        self.scroll = 0
+        self.window.bind("<Down>", self.scrolldown)
+
+    def load(self, url):
+        body = url.request()
+        text = lex(body)
+        self.display_list = layout(text)
+        self.draw()
+
+    def draw(self):
+        self.canvas.delete("all")
+        for x, y, c in self.display_list:
+            wbetools.record("draw")
+            # Skip drawing characters that are below the visible area
+            if y > self.scroll + HEIGHT: continue
+            # Skip drawing characters that are above the visible area
+            if y + VSTEP < self.scroll: continue
+            self.canvas.create_text(x, y - self.scroll, text=c)
+
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
+
+if __name__ == "__main__":
+    import sys
+
+    Browser().load(URL(sys.argv[1]))
+    tkinter.mainloop()
